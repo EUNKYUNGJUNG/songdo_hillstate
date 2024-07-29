e1d14aad094630b1e6cfa4e5b740595677f36e7d
EUNKYUNGJUNG <luckyjek@naver.com>
2024-07-29 15:49:31 +0900

    deploy test

---
+++ api/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAEV1H9Z2CRtnFWWWT1kkUWPyBbujK4Yqo",
+  authDomain: "uridongnaesongdohill.firebaseapp.com",
+  databaseURL:
+    "https://uridongnaesongdohill-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "uridongnaesongdohill",
+};
+
+const app = initializeApp(firebaseConfig);
